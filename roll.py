71389ccb5d0c7785dcd35080ab68db706890ae0c
AieryAir <104162923+AieryAir@users.noreply.github.com>
2024-05-30 00:36:14 +0800

    add advantage/disadvantage for dice rolls

--- roll.py
+++ roll.py
@@ -4,12 +4,29 @@ import random
 dice = 1
 sides = 20
 
-roll = random.randint(1, sides)
+# Roll first checks for advantage/disadvantage
+advantage = False
+disadvantage = False
+
+if advantage:
+  roll1 = random.randint(1, sides)
+  roll2 = random.randint(1, sides)
+  roll = max(roll1, roll2)
+  
+elif disadvantage:
+  roll1 = random.randint(1, sides)
+  roll2 = random.randint(1, sides)
+  roll = min(roll1, roll2)
+  
+else:
+  roll = random.randint(1, sides)
 
 print(f"You rolled {dice}d{sides} and got {roll}.") 
 
-if sides == 20 & roll == 20:
-    print ("Critical Success! It's a Natural 20!")
-    
+# Natural 1 & 20 Message Prompt
+
+if sides == 20 and roll == 20:
+  print ("Critical Success! It's a Natural 20!")
+
 elif roll == 1:
-    print ("Oh no! It's a Natural 1!")
+  print ("Oh no! It's a Natural 1!")
