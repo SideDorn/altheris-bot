bdf9785b3e0e07d40d048aa923d7076e991b04b9
AieryAir <104162923+AieryAir@users.noreply.github.com>
2024-05-30 23:09:07 +0800

    add reward messages

--- slots.py
+++ slots.py
@@ -2,16 +2,20 @@ import random
 
 def slotreward(bet, slot1, slot2, slot3):
     if slot1 == slot2 == slot3 == 7:
-        return bet * 200
+        message = "JACKPOT!!! You got three 7s! "
+        return bet * 200, message
     
     elif slot1 == slot2 == slot3:
-        return bet * 20
+        message = "Huge win!! All numbers matched! "
+        return bet * 20, message
     
     elif slot1 == slot2 or slot1 == slot3 or slot2 == slot3:
-        return bet * 2
+        message = "Congratulations! Two numbers matched! "
+        return bet * 2, message
     
     else:
-        return bet * 0
+        message = "Unfortunately you didn't win. "
+        return bet * 0, message
     
 def slotmachine(bet, slot1, slot2, slot3):
     bet = 100 # placeholder value for now
@@ -21,7 +25,7 @@ def slotmachine(bet, slot1, slot2, slot3):
     slot3 = random.randint(1, 12)
 
     print (f"{slot1} / {slot2} / {slot3}")
-    reward = slotreward(bet, slot1, slot2, slot3)
-    print (f"You earned {reward}.")
+    reward, message = slotreward(bet, slot1, slot2, slot3)
+    print (f"{message}You won {reward} gold!")
 
 slotmachine('bet', 'slot1', 'slot2', 'slot3')
